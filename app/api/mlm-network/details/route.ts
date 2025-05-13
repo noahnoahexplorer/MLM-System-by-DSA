633b605fdf37d40b71fc39fcd60232689731f5a1
RongHuiNoahGoh <ronghui_goh@blacksire.com>
2025-05-13 11:52:43 +0800

    bugs/caching-issue

--- app/api/mlm-network/details/route.ts
+++ app/api/mlm-network/details/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { executeQuery } from '@/lib/snowflake';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(
   request: Request,
   { params }: { params: Record<string, string> }
